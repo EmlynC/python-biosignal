749a9aef5883ef6561d5fe41130543d60a826aa0
Emlyn Clay <eclay101@gmail.com>
2016-06-02 12:13:19 +0100

    Initial skeleton files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='biosignal',
+      version='0.0.1',
+      description='',
+      url='http://github.com/EmlynC/python-biosignal',
+      author='Emlyn Clay',
+      author_email='eclay101@gmail.com',
+      license='MIT',
+      packages=['biosignal'],
+      zip_safe=False)
