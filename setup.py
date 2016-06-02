9bc9232f2b2b95b7031afe3b0a8ec12f9d77384a
Emlyn Clay <eclay101@gmail.com>
2016-06-02 12:32:38 +0100

    Add a description for PyPI

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup
 
 setup(name='biosignal',
-      version='0.0.1',
-      description='',
+      version='0.0.2',
+      description="""A library for processing and analysing physiological
+      signals such as ECG, BP, EMG, EEG, Pulse and Sp02""",
       url='http://github.com/EmlynC/python-biosignal',
       author='Emlyn Clay',
       author_email='eclay101@gmail.com',
